7e49c2e8340dbf1ea9fd9b488cc0157239268ba6
temniprincc <danil1233211123@yandex.ru>
2025-09-26 12:01:39 +0300

    Add files via upload

---
+++ 1.11/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _1._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите название футбольной команды: ");
+            string user = Console.ReadLine();
+            Console.WriteLine(user + " - чемпион");
+        }
+    }
+}
