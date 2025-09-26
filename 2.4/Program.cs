d20fc38cc3b6918c8465e3a585cb866eef68db77
temniprincc <danil1233211123@yandex.ru>
2025-09-26 13:25:24 +0300

    Add files via upload

---
+++ 2.4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину стороны квадрата - ");
+            int user = int.Parse(Console.ReadLine());
+            int res = user * 4;
+            Console.WriteLine("Периметр - "+res);
+        }
+    }
+}
---
+++ 2.5/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _2._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус - ");
+            double r = double.Parse(Console.ReadLine());
+            double res = 2 * r;
+            Console.WriteLine("Диаметр - "+res);
+        }
+    }
+}
